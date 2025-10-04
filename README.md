cead209f85b3a590e55daf559ea887c0ddb4e7e5
François Roland <francois.roland@heh.be>
2025-10-04 17:47:34 +0200

    Write instructions and unit test.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# base
+# Boucles - TP6 - Série harmonique
 
-Consignes
+## Consignes
+
+Créer une fonction `harmonique(n: number): number` calculant la somme des n premiers termes de la "série harmonique".
+
+## Rappels mathématiques
+
+La série harmonique d'un nombre naturel non nul n est la série des inverses des entiers naturels non nuls :
+
+$\sum^{\infty}_{n=1} \frac{1}{n} = 1 + \frac{1}{2} + \frac{1}{3} + \dots + \frac{1}{n}$.
+
+## Exemples
+
+- `harmonique(1)` $= 1$
+- `harmonique(2)` $= 1.5$
+- `harmonique(3)` $\approx 1.8$
+- `harmonique(4)` $\approx 2.1$
+
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,25 @@
 // Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
 
-import {...} from '../index';
+import {harmonique} from '../index';
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
+describe('La fonction harmonique de', () => {
+    test('1 vaut 1', () => {
+        expect(harmonique(1)).toEqual(1);
+    });
+
+    test('2 vaut 1.5', () => {
+        expect(harmonique(2)).toBeCloseTo(1.5, 2);
+    });
+
+    test('3 vaut 1.83', () => {
+        expect(harmonique(3)).toBeCloseTo(1.83, 2);
+    });
+
+    test('4 vaut 2.08', () => {
+        expect(harmonique(4)).toBeCloseTo(2.08, 2);
+    });
+
+    test('5 vaut 2.28', () => {
+        expect(harmonique(5)).toBeCloseTo(2.28, 2);
     });
 });
